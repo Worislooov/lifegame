91b747ca6b7ef26ff0b5e2ef13609b243ac87af3
Lev Kerskiy-Gosling <levker08@icloud.com>
2024-03-25 19:40:31 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -41,6 +41,9 @@ public class Main {
         }
     }
 
+
+
+
     public static int[][] playGame(int[][] board) {
         for (int i = 0; i < board.length; i++){
             for (int j = 0; j < board[i].length; j++){
